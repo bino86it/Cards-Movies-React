f08a148eb3c322159f53e7453f805e82255101e9
Jacopo Salvi <jacopo.salvi.86@gmail.com>
2024-02-01 20:14:40 +0100

    in attesa implementazione fetch su file json

---
+++ src/components/carta.jsx
@@ -0,0 +1,20 @@
+const Carta = ({title, year, imgUrl})=>{
+
+
+
+
+    return(
+    
+        <div className="box">
+            <h3>{title}</h3>
+            <h5>{year}</h5>
+            <img src={imgUrl} alt="" />
+            {Number(year)<=new Date().getFullYear() ? <span className="uscito">Uscito</span> : <span className="prossimamente">prossimamente</span>}
+    
+    
+        </div>
+    )
+    
+    }
+    
+    export default Carta;
